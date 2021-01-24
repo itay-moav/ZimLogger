aaf2bdfb40f36823fa7f83fa7dd5af323be65a5b
itay-moav <itay.malimovka@gmail.com>
2021-01-23 19:29:22 -0500

    Initial booooom

---
+++ src/ZimLogger/Streams/Nan.php
@@ -0,0 +1,12 @@
+<?php namespace Talis\Logger\Streams;
+/**
+ * Not a real logger -> this is a sink
+ * 
+ * @author itaymoav
+ */
+class Nan extends aLogStream{
+    	
+	protected function log($inp,$severity,$full_stack_data = null){
+        //abba nagila
+	}
+}
