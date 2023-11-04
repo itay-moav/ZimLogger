6c96417c74dc109cdc0d422fb7d0605bd392e17c
Itay Moav <itay.malimovka@gmail.com>
2023-11-04 17:49:55 -0400

    fix formating of message

--- src/ZimLogger/Streams/StdError.php
+++ src/ZimLogger/Streams/StdError.php
@@ -10,10 +10,12 @@ class StdError extends aLogStream{
 	 * {@inheritDoc}
 	 * @see \ZimLogger\Streams\aLogStream::log()
 	 */
-	protected function log(string $inp,int $severity,array $full_stack_data = []):void{
-        file_put_contents( 'php://stderr',"[{$severity}][".@date('h:i:s', time())."] [{$this->call_sig}] " );
-		if($full_stack_data){
-		    file_put_contents( 'php://stderr', "[FULL STACK] \n" . print_r($full_stack_data,true));
-		}
-	}
+    protected function log(string $inp,int $severity,array $full_stack_data = []):void{
+        
+        $log = "[{$severity}][".@date('h:i:s', time()) . ']' . ($this->call_sig ? "[{$this->call_sig}]" : '') . $inp;
+        file_put_contents( 'php://stderr',$log . PHP_EOL );
+        if($full_stack_data){
+                file_put_contents( 'php://stderr', '[FULL STACK]' . PHP_EOL . print_r($full_stack_data,true) . PHP_EOL);
+        } // EOF if full stack
+    }//EOF log()
 }
